b47fe4aaf4e40730f4f0970ac4e7ca1994f0cc3a
Mohammed Ali <moh_fcis@yahoo.com>
2020-06-25 15:30:02 +0000

    version 0.1.0

---
+++ R/covid19dbcand.R
@@ -0,0 +1,19 @@
+#' covid19dbcand: A data package that contains different datasets extracted 
+#' from \strong{DrugBank} xml database.
+#'
+#' The datasets in `covid19dbcand` is related to 
+#' [DrugBank](https://www.drugbank.ca/covid-19#drugs) selected drugs for
+#' covid-19
+#' 
+#' The package is a smaller version from 
+#' [dbdataset package](https://github.com/Dainanahan/dbdataset).
+#' 
+#' It is extracted using [dbparser](https://dainanahan.github.io/dbparser/) 
+#' 
+#' For more information kindly check the
+#' reference/index 
+#' (https://mohammedfcis.github.io/covid19dbcand/reference/index.html)
+#'
+#' @docType package
+#' @name covid19dbcand
+NULL
